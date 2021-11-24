cbde5560386836f2a186bf77b5f5a79efeeeb3af
toutpuissantged <amoussougedeon13@gmail.com>
2021-11-24 17:14:44 +0000

    mis en place de lago de base

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+from src.core.chaine import Blockchaine 
+from time import time
 
+Chaine = Blockchaine()
 
-class Block
+for i in range(100) :
+    t1 = time() 
+    block = Chaine.get_previous_block()
+    new_prof = Chaine.proof_of_work(block['proof'])
+    new_block = Chaine.create_block(new_prof, Chaine.hash(block))
+    t2 = time()
+    print(f'{i} : {t2-t1}')
+
+print(Chaine.is_chain_valid(Chaine.chain))
